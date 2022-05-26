96ede316a641679a038c097c94ffa0a95ac38601
Natalia29011989 <tjomamon@gmail.com>
2022-05-26 12:04:11 +0300

    1 дз

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const start = document.querySelector('button[data-start]')
+const stop = document.querySelector('button[data-stop]')
+const body = document.querySelector("body")
 
+let IsDisabled = true;
+let timerId = null;
+
+start.addEventListener('click', startColor)
+
+stop.addEventListener('click', stopColor)
+
+function startColor() {
+       if (!IsDisabled) {
+        return;
+    }
+   
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+
+    }, 1000,);
+
+    IsDisabled = false;
+}
+
+function stopColor() {
+        if (IsDisabled) {
+        return;
+    }
+    clearInterval(timerId); 
+   
+     IsDisabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
